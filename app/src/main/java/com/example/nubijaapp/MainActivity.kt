48384feb911b170a132746baef90f63a59abfffa
SeugnshinLee <lsver67@gmail.com>
2020-12-09 13:55:49 +0900

    BotNav : MainActivity - onBackPressed() insert Log code

--- app/src/main/java/com/example/nubijaapp/MainActivity.kt
+++ app/src/main/java/com/example/nubijaapp/MainActivity.kt
@@ -73,6 +73,9 @@ class MainActivity : AppCompatActivity() {
     // 뒤로가기 버튼 이 눌러졌을때
     override fun onBackPressed() {
         if (System.currentTimeMillis() - mBackWait >= 2000){
+
+            //디버깅을 위해 로그 삽입
+            Log.d(TAG, "MainActivity - onBackPressed() called")
             mBackWait = System.currentTimeMillis()
             Toast.makeText(this, "종료하시려면 뒤로가기 버튼을 한번 더 눌러주세요.", Toast.LENGTH_SHORT).show()
         }
