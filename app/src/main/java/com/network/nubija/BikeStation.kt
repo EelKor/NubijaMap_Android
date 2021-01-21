b2e9e9fcc3d98569d233c07b5d2ec7ac17e5e7d8
SeugnshinLee <lsver67@gmail.com>
2021-01-21 12:05:00 +0900

    RTBikeStation 데이터 클래스 삭제 ( 불필요 )

--- app/src/main/java/com/network/nubija/BikeStation.kt
+++ app/src/main/java/com/network/nubija/BikeStation.kt
@@ -1,9 +1,5 @@
-package com.underbar.nubijaapp
+package com.network.nubija
 
 data class BikeStation(
         var name: String, var lat: Double, var lng: Double, var tmid: Int, var empty: Int, var park: Int
-)
-
-data class BikeStationRT(
-        var vno:Int, var park:Int, var empty:Int
 )
