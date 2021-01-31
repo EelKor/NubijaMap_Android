cd76e75221b6748c886a8dfbf29035ee8c1badcd
SeugnshinLee <lsver67@gmail.com>
2021-01-31 22:44:27 +0900

    버전 표시 및 임포트 최적화

--- app/src/main/java/com/underbar/nubijaapp/MainActivity.kt
+++ app/src/main/java/com/underbar/nubijaapp/MainActivity.kt
@@ -9,7 +9,6 @@ import android.content.res.AssetManager
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Vibrator
 import android.provider.Settings
 import android.widget.Toast
 import androidx.annotation.RequiresApi
