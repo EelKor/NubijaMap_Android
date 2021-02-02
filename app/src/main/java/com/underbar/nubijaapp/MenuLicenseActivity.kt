e37a107c5e362f52b6d874a70c4ecf080cde2763
SeugnshinLee <lsver67@gmail.com>
2021-02-02 22:11:44 +0900

    오픈소스 라이센스 페이지 형식 제작

---
+++ app/src/main/java/com/underbar/nubijaapp/MenuLicenseActivity.kt
@@ -0,0 +1,11 @@
+package com.underbar.nubijaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuLicenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_license)
+    }
+}
