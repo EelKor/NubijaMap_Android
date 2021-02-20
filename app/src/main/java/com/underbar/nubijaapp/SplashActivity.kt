ae3b0f51ecb216ea5a7ab41e078ca25aa058b48b
SeugnshinLee <lsver67@gmail.com>
2021-02-20 22:21:04 +0900

    스플래시 시간 축소 1500ms -> 1000ms

--- app/src/main/java/com/underbar/nubijaapp/SplashActivity.kt
+++ app/src/main/java/com/underbar/nubijaapp/SplashActivity.kt
@@ -7,7 +7,7 @@ import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
-    val SPLASH_TIME:Long = 1500
+    val SPLASH_TIME:Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
