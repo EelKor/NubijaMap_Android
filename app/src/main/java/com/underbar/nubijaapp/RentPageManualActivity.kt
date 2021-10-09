4033ec857fe863c4bbaaec2dae8a454a0958ba84
DESKTOP-HPL8OJ0\이승신 <lsver67@gmail.com>
2021-10-09 13:50:10 +0900

    누비자 대여 가이드 페이지 프래그먼트 레이아웃 구축

--- app/src/main/java/com/underbar/nubijaapp/RentPageManualActivity.kt
+++ app/src/main/java/com/underbar/nubijaapp/RentPageManualActivity.kt
@@ -4,23 +4,83 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.underbar.nubijaapp.databinding.ActivityMainBinding
+import com.underbar.nubijaapp.databinding.ActivityRentPageBinding
+import com.underbar.nubijaapp.databinding.ActivityRentPageManualBinding
 
 class RentPageManualActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRentPageManualBinding
+    
+    // 프래그먼트 페이지 인덱스
+    private var page: Int = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rent_page_manual)
+        
+        // 뷰 바인딩
+        binding = ActivityRentPageManualBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
+        // 초기 프래그먼트 로딩
+        setFragment()
+        
+        // 다음 버튼 누르면 프래그먼트 전환
+        binding.btnNext.setOnClickListener {
+            page += 1
 
-        val Before: Button = findViewById(R.id.btnBefore)
-        val Next: Button = findViewById(R.id.btnNext)
+            if (page > 3)   {
+                val rentPage = Intent(this, RentPageActivity::class.java)
+                startActivity(rentPage)
+                finish()
+            }
+            else
+                replaceFragment(page)
 
-        Before.setOnClickListener {
-            finish()
+        }
+        
+        // 이전 프래그먼트로 전환
+        binding.btnBefore.setOnClickListener {
+            page -= 1
+
+            if (page < 1)
+                finish()
+            else
+                replaceFragment(page)
+        }
+    }
+
+    private fun setFragment()   {
+        val transaction = supportFragmentManager.beginTransaction()
+            .add(R.id.frameLayout, ManualPage1Fragment())
+
+        transaction.commit()
+    }
+
+    private fun replaceFragment(page: Int)  {
+        when(page)  {
+
+            1 -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, ManualPage1Fragment())
+                transaction.commit()
+            }
+
+            2 -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, ManualPage2Fragment())
+                transaction.commit()
+            }
+
+            3 -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, ManualPage3Fragment())
+                transaction.commit()
+            }
         }
 
-        Next.setOnClickListener {
-            val intent = Intent( this, RentPageActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
 }
